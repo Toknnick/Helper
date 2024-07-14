6fb6c6b2262c6cd0e8d047a8e525b2e7247c4322
Toknnick <Toknnick@yandex.ru>
2024-07-14 13:41:33 +0300

    добавлен выбор создания
    добавил выбор создоваемого, есть создание текста для scrollView. Все работает корректно

--- app/src/main/java/com/example/helper1/HomeFragment.kt
+++ app/src/main/java/com/example/helper1/HomeFragment.kt
@@ -1,59 +1,41 @@
 package com.example.helper1
 
+import android.annotation.SuppressLint
+import android.graphics.Color
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import com.example.helper1.databinding.FragmentHomeBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class HomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+class HomeFragment :  Fragment(){
+    private lateinit var binding: FragmentHomeBinding
+    private lateinit var mainActivity: MainActivity
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View {
+        binding = FragmentHomeBinding.inflate(inflater)
+        //createSpinner()
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mainActivity = (activity as MainActivity)
+        binding.addButton.setOnClickListener{
+            mainActivity.showDialog(this)
+        }
+    }
+
+    @SuppressLint("ResourceAsColor")
+    fun getText(newText: TextView){
+        binding.layout.addView(newText)
+        Log.v("MyTag","color"+newText.background)
     }
 }
--- app/src/main/java/com/example/helper1/MainActivity.kt
+++ app/src/main/java/com/example/helper1/MainActivity.kt
@@ -1,30 +1,103 @@
 package com.example.helper1
 
+import android.content.Context
+import android.content.pm.ActivityInfo
+import android.graphics.Color
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.LinearLayout
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.FragmentManager
 import androidx.navigation.NavController
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI.setupWithNavController
-import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.helper1.databinding.ActivityMainBinding
-import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private  lateinit var navController: NavController
+    private lateinit var navController: NavController
+    private lateinit var fragment: HomeFragment
+    private lateinit var fm: FragmentManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+        this.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED;
         setContentView(binding.root)
+        fm = supportFragmentManager
 
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.mainContainer) as NavHostFragment
+        val navHostFragment = fm.findFragmentById(R.id.mainContainer) as NavHostFragment
         navController = navHostFragment.navController
         val bottomNavigationView = binding.bottomNavigationView
         setupWithNavController(bottomNavigationView,navController)
+    }
+
+    fun getContext():Context{
+        return this
+    }
+
+    //TODO: добавить возможность менять цвет в настройки
+    private fun createText(text: String, color: Int): TextView {
+        val textView = TextView(this)
+        textView.setBackgroundColor(Color.GREEN)
+        textView.textSize = 22F
+        textView.text = text
+        textView.setLayoutParams(
+            LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+            )
+        )
+
+        return textView
+    }
+
+    fun showDialog(fragment: HomeFragment){
+        val langArray: Array<String> = arrayOf("Задача", "Событие")
+        val selectedEvent = BooleanArray(langArray.size)
+        val langList: ArrayList<Int> = ArrayList()
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+        builder.setTitle("Выбрерите тип")
+        builder.setCancelable(false)
+
+        builder.setMultiChoiceItems(langArray, selectedEvent
+        ) { dialogInterface, i, b ->
+            if (b) {
+                langList.add(i)
+                langList.sort()
+            } else {
+                langList.remove(i)
+            }
+        }
+
+        builder.setPositiveButton("OK"
+        ) { dialogInterface, i -> // Initialize string builder
+            if (langList.size != 1) {
+                val stringBuilder = StringBuilder()
+                for (j in langList.indices) {
+                    stringBuilder.append(langArray[langList[j]])
+                    if (j != langList.size - 1) {
+                        stringBuilder.append(", ")
+                    }
+                }
+                Toast.makeText(applicationContext,"Ошибка! Необходимо выбрать что-то одно",Toast.LENGTH_LONG).show()
+            } else {
+                if (langList[0] == 0) {
+                    //Делаем задачу
+                } else {
+                    //Делаем событие
+                    fragment.getText(createText("HELLO!", R.color.green))
+                }
+            }
+        }
+
+        builder.setNegativeButton("Cancel"
+        ) { dialogInterface, i -> // dismiss dialog
+            dialogInterface.dismiss()
+        }
+
+        builder.show()
     }
 }
