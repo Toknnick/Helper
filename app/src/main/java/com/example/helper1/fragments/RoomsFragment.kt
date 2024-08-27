c699683f78921ec70c4cf814f18614d14a43d19b
Toknnick <Toknnick@yandex.ru>
2024-08-27 13:45:26 +0300

    откат прошлых изменений

--- app/src/main/java/com/example/helper1/fragments/RoomsFragment.kt
+++ app/src/main/java/com/example/helper1/fragments/RoomsFragment.kt
@@ -1,7 +1,32 @@
 package com.example.helper1.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.helper1.databinding.FragmentRoomsBinding
 
-@Suppress("DEPRECATION", "NAME_SHADOWING")
 class RoomsFragment : Fragment() {
+    private lateinit var binding: FragmentRoomsBinding
+    //TODO: менять у нынешнего пользователя availableRooms после подключения к комнате
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentRoomsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.saveUserButton.setOnClickListener {
+            createUser()
+        }
+        binding.updateUserButton.setOnClickListener {
+            updateUser()
+        }
+    }
+
 }
